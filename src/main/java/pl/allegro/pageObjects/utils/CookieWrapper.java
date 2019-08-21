9db41feb8062b03cb60dc14ff430d2177b71fc16
Miczura <m.gieleta@gmail.com>
2019-08-21 16:13:11 +0200

    Added a CookieWrapper class

---
+++ src/main/java/pl/allegro/pageObjects/utils/CookieWrapper.java
@@ -0,0 +1,17 @@
+package pl.allegro.pageObjects.utils;
+
+import org.openqa.selenium.Cookie;
+import org.openqa.selenium.WebDriver;
+
+import java.util.Date;
+
+
+public class CookieWrapper {
+    //Friday, August 21, 2020 1:58:31 PM
+    public static final long epochInMilis =1598018311000L;
+
+    public static void addCookie(WebDriver driver,String name, String value, String domain, String path) {
+        driver.manage().addCookie(
+                new Cookie(name, value, domain, path, new Date(epochInMilis)));
+    }
+}
