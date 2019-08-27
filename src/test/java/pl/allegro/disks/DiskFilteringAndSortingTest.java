575092d093e82263f638e66510f934bf5ea70526
Miczura <m.gieleta@gmail.com>
2019-08-27 18:53:42 +0200

    Added a Test

--- src/test/java/pl/allegro/disks/DiskFilteringAndSortingTest.java
+++ src/test/java/pl/allegro/disks/DiskFilteringAndSortingTest.java
@@ -1,36 +1,66 @@
 package pl.allegro.disks;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
+
+
 import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import pl.allegro.pageObjects.pages.ExternalAndPortableDiscsPage;
 import pl.allegro.pageObjects.pages.MainPage;
 
+
 public class DiskFilteringAndSortingTest {
-    private MainPage mainpage  = new MainPage(driver);
     private static WebDriver driver;
+    private ExternalAndPortableDiscsPage externalAndPortableDiscsPage;
+    MainPage mainpage = new MainPage(driver);
 
     @BeforeClass
-    public static void setupTest(){
+    public static void setup(){
         WebDriverManager.chromedriver().setup();
         driver = new ChromeDriver();
+
     }
     @Before
-    public void openAllegro(){
-        mainpage.navigateToMainPage();
+    public void navigateToExternalAndPortableDiscs()  {
+        //given
+        navigateToExternalAndPortableDiscPage();
     }
 
     @Test
     public void test() throws InterruptedException {
-        Thread.sleep(2000);
+        //when
+      externalAndPortableDiscsPage.getExternalAndPortableDiscsMenu()
+              .setupfilteringDiscCapacity("200","300")
+              .selectSortingMethod();
+        //then checking if prices are sorted descending
+       externalAndPortableDiscsPage.getExternalAndPortableDiscsMenu()
+              .checkIfPricesAreSorted(externalAndPortableDiscsPage
+                      .getExternalAndPortableDiscsMenu()
+                      .getListOfPrices());
+
     }
 
+
     @AfterClass
-    public static void tearDown(){
+    public static void teardown(){
         driver.close();
         driver.quit();
     }
+
+    private void navigateToExternalAndPortableDiscPage(){
+        externalAndPortableDiscsPage= mainpage.navigateToMainPage().getMainCategoriesMenu()
+                .selectElectronicsLink()
+                .getElectronicsMenu()
+                .selectComputersLink()
+                .getComputersMenu()
+                .selectPortableDiscAndMemoriesPage()
+                .getPortableDiscsAndMemoriesMenu()
+                .selectExternalAndPortableDiscsLink();
+    }
+
+
 }
